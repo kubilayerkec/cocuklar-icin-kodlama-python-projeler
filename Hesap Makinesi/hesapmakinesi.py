5c3ee423aacad91753543dd26d1bb21222cf5a81
Kubilay Erkeç <kubilay@kubilay.net>
2020-06-05 16:49:27 +0300

    code changed for main block

--- Hesap Makinesi/hesapmakinesi.py
+++ Hesap Makinesi/hesapmakinesi.py
@@ -2,19 +2,23 @@
 # kubilay@kubilay.net
 # Udemy Eğitimleri: https://www.udemy.com/user/kubilay-erkec/
 
-print("İşlemler:","1 -> Toplama","2 -> Çıkarma", "3 -> Bölme", "4 -> Çarpma", sep="\n")
-sayi1 = int(input("İşlem yapılacak ilk sayıyı giriniz: "))
-sayi2 = int(input("İşlem yapılacak ikinci sayıyı giriniz: "))
-islem = int(input("İşlemin ne olduğunu giriniz: "))
+def main():
+    print("İşlemler:", "1 -> Toplama", "2 -> Çıkarma", "3 -> Bölme", "4 -> Çarpma", sep="\n")
+    sayi1 = int(input("İşlem yapılacak ilk sayıyı giriniz: "))
+    sayi2 = int(input("İşlem yapılacak ikinci sayıyı giriniz: "))
+    islem = int(input("İşlemin ne olduğunu giriniz: "))
 
-sonuc = 0
-if islem == 1:
-    sonuc = sayi1 + sayi2
-elif islem == 2:
-    sonuc = sayi1 - sayi2
-elif islem == 3:
-    sonuc = sayi1 / sayi2
-elif islem == 4:
-    sonuc = sayi1 * sayi2
+    sonuc = 0
+    if islem == 1:
+        sonuc = sayi1 + sayi2
+    elif islem == 2:
+        sonuc = sayi1 - sayi2
+    elif islem == 3:
+        sonuc = sayi1 / sayi2
+    elif islem == 4:
+        sonuc = sayi1 * sayi2
 
-print("işlem sonucu: ", sonuc)
+    print("işlem sonucu: ", sonuc)
+
+if __name__ == '__main__':
+    main()
--- Karenin Alanını Hesaplayan Program/kareninalani.py
+++ Karenin Alanını Hesaplayan Program/kareninalani.py
@@ -2,9 +2,13 @@
 # kubilay@kubilay.net
 # Udemy Eğitimleri: https://www.udemy.com/user/kubilay-erkec/
 
-kenar = int(input("Karenin bir kenarının ölçüsünü giriniz: "))
-if kenar <= 0:
-    print("Karenin kenarı sıfır veya sıfırdan küçük olamaz")
-else:
-    alanhesabi = kenar * kenar
-    print("Karenin alanı", alanhesabi,sep=" = ")
+def main():
+    kenar = int(input("Karenin bir kenarının ölçüsünü giriniz: "))
+    if kenar <= 0:
+        print("Karenin kenarı sıfır veya sıfırdan küçük olamaz")
+    else:
+        alanhesabi = kenar * kenar
+        print("Karenin alanı", alanhesabi, sep=" = ")
+
+if __name__ == '__main__':
+    main()
